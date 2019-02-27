2820c123d2a229fba3f580f8b49abe97ee4667f1
aadesh48 <aadesh.srivastava_ccv17@gla.ac.in>
2019-02-27 10:15:16 +0530

    singlylinkedlist

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,47 +1,110 @@
 package main;
-public class SinglyLinkedList {
-    public class Node{
+public class SinglyLinkedList
+{
+    private class Node
+    {
         private int data;
-        private Node next;
+        Node next;
 
-
-        public int getData()
-        {
+        public int getData() {
             return data;
         }
-        public int getNext()
-        {
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
             return next;
         }
-        public void setData(int data)
-        {
-            this.data=data;
-        }
-        public void setNext(Node next)
-        {
-            this.next=next;
-        }
-    public node(int data)
-{   this.data=data;
-}
-        public node(int data,Node next)
-        {   this.data=data;this.next=next;
-        }
-}
-private Node head=null;
-private int size=0;
-public static void main(String args[])
-{ SinglyLinkedList linkedList=new SinglyLinkedList();
-    System.out.println(linkedlist);
-}
-public String toString(){
-  StringBuilder response = new StringBuilder();
 
-response.append("[");Node temp=this.head;while(temp!=null){response.append(temp.getData());temp=temp.next;}
-    response.append("]");
+        public void setNext(Node next) {
+            this.next = next;
+        }
 
-return response.toString();
-} private void insertHead(int data){Node newNode=new Node(data,this.head);this.head=newNode
-}
-private void insertAfter(int data,Node node){
-node.next=new Node(data,node.next);size++;           }
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node() {
+            this.data = 0;
+            this.next = null;
+        }
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next=null;
+        }
+    }
+
+    private Node head=null;
+    private int size=0;
+
+    @Override
+    public String toString()
+    {
+        StringBuilder response= new StringBuilder();
+        response.append("[");
+        Node temp=this.head;
+        while(temp!=null)
+        {
+            response.append(temp.getData());
+            if(temp.getNext()!=null) {
+                response.append(" ==> ");
+            }
+            temp=temp.getNext();
+        }
+        response.append("]");
+        return response.toString();
+    }
+
+    private  void insert_Head(int data)
+    {
+        Node new_Node=new Node(data,this.head);
+        this.head=new_Node;
+        size++;
+
+
+    }
+    private void insert_After(int data,Node node)
+    {
+        Node new_node=new Node(data,node.next);
+        node.next=new_node;
+        size++;
+    }
+
+    public void insert(int data)
+    {
+        if(head==null)
+        {
+            insert_Head(data);
+        }
+        else
+        {
+            Node temp=this.head;
+            while(temp.next!=null)
+            {
+                temp=temp.next;
+            }
+            insert_After(data,temp);
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        Singlelinkedlist linkedlist=new Singlelinkedlist();
+        System.out.println(linkedlist);
+        for (int i = 0; i <5 ; i++)
+        {
+            linkedlist.insert(i+2);
+        }
+        linkedlist.insert(42);
+        linkedlist.insert(40);
+        System.out.println(linkedlist);
+
+
+    }
+         }
